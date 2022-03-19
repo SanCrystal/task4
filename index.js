400283d122d3b11b9b12d0ca63b631e2c8e33f84
sanCrystal <anyanwu.amanzearthur@gmail.com>
2022-03-19 20:50:03 +0000

    port changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require('path')
 require("dotenv").config();
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 //initialize express
 const app = express();
 
